3cec7ab77fa2c9b6ddf973da11679f51056d2d37
miguelmejia10 <64266317+MiguelMejia10@users.noreply.github.com>
2022-01-18 22:00:20 -0500

    Prueba de Git en tu repo
    te saldrá un error al realizar push

---
+++ debeshacerpull.java
@@ -0,0 +1,5 @@
+public class HolaMundo {
+  public static void main (String[] args) {
+    System.out.println("Debes hacer un pull por que tienes que actualizar tu repo local antes de subir cambios a un repositorio ya que tiene un cambio nuevo!");
+  }
+}
